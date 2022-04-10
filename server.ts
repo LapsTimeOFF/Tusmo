502f64757f1f878a6bc6b8af3fa16f871a8d21cd
LapsTime <39061148+LapsTimeOFF@users.noreply.github.com>
2022-04-10 20:36:27 +0200

    Adding CSS & TS file and adding url get this files

--- server.ts
+++ server.ts
@@ -10,6 +10,14 @@ const port: number = 1345;
 app.get('/', (_, res) => {
     res.sendFile(`${__dirname}/html/index.html`)
 })
+app.get('/css/:name', (req, res) => {
+    const {name} = req.params
+    res.sendFile(`${__dirname}/css/${name}.css`)
+})
+app.get('/ts/:name', (req, res) => {
+    const {name} = req.params
+    res.sendFile(`${__dirname}/ts/${name}.ts`)
+})
 
 app.listen(port, () => {
     log(`[API OK] Ecoute sur http://localhost:`+port);
